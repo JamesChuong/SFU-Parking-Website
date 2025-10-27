f894fda84bafe246043ffcbcaa9c28ced49c5fd6
James Chuong <jcjameschuong@gmail.com>
2025-10-26 18:48:01 -0700

    Set up frontend

---
+++ client/SFU Parking App/src/axios_config.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+// @ts-ignore
+import { useSelector } from 'react-redux';
+// @ts-ignore
+import { AuthState } from 'redux_store/store.ts'
+
+const axios_config = axios.create({
+  baseURL: 'http://localhost:8000',
+  withCredentials: true
+
+});
+
+// axios_config.defaults.headers.common['Authorization'] = useSelector((state: AuthState) => state.authentication.token);
+
+axios_config.interceptors.request.use(config => {
+    const token = useSelector((state: AuthState) => state.authentication.token);
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
+axios_config.defaults.headers.common['Content-Type'] = 'application/json'
+
+export default axios_config
