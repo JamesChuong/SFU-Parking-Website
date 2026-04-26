9f5a5d5f0b85920688a1ab2e8388a3e77cc7e809
James Chuong <jcjameschuong@gmail.com>
2026-04-25 20:52:39 -0700

    Remove old backend

--- server/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Course, NonLectureSection, LectureSection
-
-
-admin.site.register(User)
-admin.site.register(Course)
-admin.site.register(NonLectureSection)
-admin.site.register(LectureSection)
